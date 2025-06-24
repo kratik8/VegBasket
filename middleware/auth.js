90fe2c61bd78e16a811157ee5979fac1bde882eb
kratik8 <krapatidar818@gmail.com>
2025-06-24 19:38:24 +0530

    Remove all files from GitHub

--- middleware/auth.js
+++
@@ -1,9 +0,0 @@
-function isAuthenticated(req, res, next) {
-  if (req.session.userId) {
-    return next(); // user is logged in, continue to the route
-  }
-  req.session.redirectTo = req.originalUrl;
-  res.redirect('/login'); // redirect if not logged in
-}
-
-module.exports = { isAuthenticated };
